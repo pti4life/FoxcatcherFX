2df2798378ca3b0b972673cc52d70f7c2bbb9769
Roland Kallai <kallai.roland@outlook.com>
2019-05-15 19:59:18 +0200

    site

---
+++ src/test/java/StateTest.java
@@ -0,0 +1,191 @@
+package org.unideb;
+
+import org.junit.jupiter.api.Test;
+
+import javax.validation.constraints.AssertFalse;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    private boolean containsForEnabledOperators(List<String> source, List<String> target) {
+        for (String str:source) {
+            if (!target.contains(str)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private boolean steppingTestHelper(int fromX, int fromY, int ToX, int ToY,State state,String figureFrom) {
+        String figureTo="";
+        if(!state.enabledOperators(fromX,fromY).isEmpty()) {
+            state.stepping(ToX,ToY);
+        }
+        List<String> li=state.getStateOfGame();
+        return li.get((ToX+1)*8-(8-ToY)).equals(figureFrom) && li.get((fromX+1)*8-(8-fromY)).equals(figureTo) ;
+
+    }
+
+
+    @Test
+    void testStepping() {
+        State state=new State(new int[][] {
+                {0,0,3,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,4,0,4,0,4,0,0}, });
+
+        assertTrue(steppingTestHelper(7,1,6,0,state,"4"));
+        assertFalse(steppingTestHelper(6,0,5,1,state,"4"));
+
+        assertTrue(steppingTestHelper(0,2,1,3,state,"3"));
+        assertFalse(steppingTestHelper(1,3,2,2,state,"3"));
+
+        assertTrue(steppingTestHelper(6,0,5,1,state,"4"));
+        assertTrue(steppingTestHelper(1,3,0,5,state,"3"));
+
+        assertTrue(steppingTestHelper(7,3,6,4,state,"4"));
+        assertTrue(steppingTestHelper(0,5,1,3,state,"3"));
+    }
+
+    @Test
+    void testEnabledOperators() {
+        State state=new State(new int[][] {
+                {0,0,3,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,4,0,4,0,4,0,0}, });
+        assertTrue(containsForEnabledOperators(List.of("btn11","btn13"),state.enabledOperators(0,2)));
+        assertTrue(containsForEnabledOperators(List.of(),state.enabledOperators(0,2)));
+        assertTrue(containsForEnabledOperators(List.of("btn62","btn64"),state.enabledOperators(7,3)));
+        assertTrue(containsForEnabledOperators(List.of(),state.enabledOperators(7,3)));
+
+        state=new State(new int[][] {
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,3,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,4,0,0,0,0,0,4},
+                {4,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0}, });
+
+
+        assertTrue(containsForEnabledOperators(List.of("btn22","btn24","btn02","btn04"),state.enabledOperators(1,3)));
+        assertTrue(containsForEnabledOperators(List.of(),state.enabledOperators(6,0)));
+        assertTrue(containsForEnabledOperators(List.of("btn46"),state.enabledOperators(5,7)));
+        state=new State(new int[][] {
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,3,0,0,0,0,0},
+                {0,4,0,0,0,0,0,4},
+                {4,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0}, });
+
+        assertTrue(containsForEnabledOperators(List.of("btn53","btn31","btn33"),state.enabledOperators(4,2)));
+
+    }
+
+    @Test
+    void testIsGoal() {
+        State state=new State(new int[][] {
+                {0,0,3,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,4,0,4,0,4,0,0}, });
+
+
+        assertFalse(state.isGoal());
+
+        state=new State(new int[][] {
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,4,0},
+                {0,0,0,0,0,3,0,0},
+                {4,0,0,0,0,0,0,0},
+                {0,0,0,4,0,0,0,0}, });
+
+
+        assertFalse(state.isGoal());
+
+        state=new State(new int[][] {
+                {3,0,0,0,0,0,0,0},
+                {0,4,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {4,0,0,0,0,0,0,0},
+                {0,0,0,4,0,0,0,0}, });
+        state.addTwoGamer(Gamer.builder().name("Gyuszi").score(0).build(),Gamer.builder().score(0).name("Pist").build());
+
+        assertTrue(state.isGoal());
+
+        state=new State(new int[][] {
+                {0,0,0,0,0,0,0,0},
+                {0,4,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,4,0,0},
+                {4,0,0,0,0,0,0,0},
+                {0,0,0,3,0,0,0,0}, });
+        state.addTwoGamer(Gamer.builder().name("Gyuszi").score(0).build(),Gamer.builder().score(0).name("Pist").build());
+
+        assertTrue(state.isGoal());
+
+    }
+
+    @Test
+    void testIsValid() {
+
+         assertThrows(IllegalArgumentException.class,()->new State(new int[][] {
+                 {0,0,3,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,4,0,4,0,4,0,0}, }));
+
+
+        assertThrows(IllegalArgumentException.class,()->new State(new int[][] {
+                {0,0,3,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,12,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,4,0,4,0,4,0,0}, }));
+
+        assertThrows(IllegalArgumentException.class,()->new State(null));
+
+        assertThrows(IllegalArgumentException.class,()->new State(new int[][] {
+                {0,0,3,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},}));
+
+    }
+}
